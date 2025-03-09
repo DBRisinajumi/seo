bde2cf0ef5aa2d68f9595bd93e1c2454bd48d703
Lauris Mierkalns <latpages@gmail.com>
2025-03-09 08:47:49 +0200

    First version

--- src/Seo.php
+++ src/Seo.php
@@ -2,22 +2,57 @@
 namespace DBRisinajumi\Seo;
 
 use Spatie\SchemaOrg\AboutPage;
+use Spatie\SchemaOrg\BreadcrumbList;
+use Spatie\SchemaOrg\ContactPage;
+use Spatie\SchemaOrg\ContactPoint;
+use Spatie\SchemaOrg\ListItem;
+use Spatie\SchemaOrg\Schema;
 
 class Seo
 {
-    private $scheme;
+    private array $schemes = [];
     
-    public function about()
+    public function about(array $data)
     {
-        $this->scheme = new AboutPage();
+        $aboutPage = Schema::aboutPage();
+        foreach ($data as $key => $value) {
+            $aboutPage->setProperty($key, $value);
+        }
+        $this->schemes[AboutPage::class] = $aboutPage;
+        return $this;
+    }
+    public function contact($contactPoints = [])
+    {
+        $contactPage = new ContactPage();
+        foreach ($contactPoints as $data) {
+            $contactPoint = new ContactPoint();
+            $contactPoint->addProperties($data);
+            $contactPage->contactPoint($contactPoint);
+        }
+        $this->schemes[ContactPage::class] = $contactPage;
+        return $this;
+    }
+    public function breadcrumbs($items = [])
+    {
+        $breadcrumbList = Schema::breadcrumbList();
+        $listItems = [];
+        foreach ($items as $data) {
+            $listItem = new ListItem();
+            $listItem->name($data['label'] ?? '');
+            if (!empty($data['url'])) {
+                $listItem->url($data['url'] ?? '');
+            }
+            $listItems[] = $listItem;
+        }
+        $breadcrumbList->itemListElement($listItems);
+        $this->schemes[BreadcrumbList::class] = $breadcrumbList;
+        return $this;
     }
     
     public function script()
     {
-        $this->scheme->toScript();
-    }
-    public function __set($property, $values)
-    {
-        $this->scheme->{$property} = $value;
+        foreach($this->schemes as $scheme) {
+            echo $scheme->toScript();
+        }
     }
 }
